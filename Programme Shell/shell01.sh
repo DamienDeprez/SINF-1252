e285ba4a790c3de829f7439ba8d39691b012cf75
DamienDeprez <damien.deprez@student.uclouvain.be>
2015-02-06 14:06:50 +0100

    Add shell 1 et 2

---
+++ Programme Shell/shell01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PROG="SINF"
+COUR=1252
+echo $PROG$COUR
---
+++ Programme Shell/shell02.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Vous avez passez" $#" arguments"
+echo "Le permier argument est : "$1
+echo "Liste des arguments : "$@
